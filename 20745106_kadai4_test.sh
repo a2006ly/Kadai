0852adfc164205900282fe6053a079182daa0bc5
yanliling <yanliling255@126.com>
2020-06-29 23:34:00 +0900

    error msg update

--- 20745106_kadai4_test.sh
+++ 20745106_kadai4_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set +e
+
 ############################################
 #
 #テストシェル
@@ -11,6 +11,8 @@ set +e
 #
 ############################################
 
+#コマンドの返り値が非ゼロのとき停止しないように
+set +e
 
 #case 1 正常系 数字入力
 #入力：引数1=15、引数2=5
@@ -19,7 +21,7 @@ set +e
 if [ $? -ne 0 ];then
     echo -e "CASE1処理エラー"  
 else
-    echo -e "CASE1正常終了"	
+    echo -e "CASE1の実行結果は想定通り"	
 fi
 echo -e "\n"
 
@@ -30,7 +32,7 @@ echo -e "\n"
 if [ $? -ne 0 ];then
     echo -e "CASE2処理エラー"  
 else
-    echo -e "CASE2正常終了"	
+    echo -e "CASE2の実行結果は想定通り"	
 fi
 echo -e "\n"
 
@@ -41,7 +43,7 @@ echo -e "\n"
 if [ $? -ne 0 ];then
     echo -e "CASE3処理エラー"  
 else
-    echo -e "CASE3正常終了"	
+    echo -e "CASE3の実行結果は想定通り"	
 fi
 echo -e "\n"
 
@@ -50,9 +52,9 @@ echo -e "\n"
 #想定出力:自然数チェックエラー
 ./20745106_kadai4.sh a 42 
 if [ $? -ne 0 ];then
-    echo -e "CASE4処理エラー"  
+    echo -e "シェル異常終了　CASE4の実行結果は想定通り"    
 else
-    echo -e "CASE4正常終了"	
+    echo -e "シェル正常終了　CASE4処理エラー"
 fi
 echo -e "\n"
 
@@ -61,9 +63,10 @@ echo -e "\n"
 #想定出力:自然数チェックエラー
 ./20745106_kadai4.sh 8 b
 if [ $? -ne 0 ];then
-    echo -e "CASE5処理エラー"  
-else
-    echo -e "CASE5正常終了"	
+    echo -e "シェル異常終了　CASE5の実行結果は想定通り" 
+ else
+    echo -e "シェル正常終了　CASE5処理エラー"  
+
 fi
 echo -e "\n"
 
@@ -72,9 +75,9 @@ echo -e "\n"
 #想定出力:自然数チェックエラー
 ./20745106_kadai4.sh c d
 if [ $? -ne 0 ];then
-    echo -e "CASE6処理エラー"  
+    echo -e "シェル異常終了　CASE6の実行結果は想定通り" 
 else
-    echo -e "CASE6正常終了"	
+    echo -e "シェル正常終了　CASE6処理エラー" 	
 fi
 echo -e "\n"
 
@@ -83,10 +86,10 @@ echo -e "\n"
 #想定出力:引数チェックエラー
 ./20745106_kadai4.sh
 if [ $? -ne 0 ];then
-    echo -e "CASE7処理エラー"  
+    echo -e "シェル異常終了　CASE7の実行結果は想定通り"  
 
 else
-    echo -e "CASE7正常終了"	
+    echo -e "シェル正常終了　CASE7処理エラー" 	
 fi
 echo -e "\n"
 
@@ -95,8 +98,8 @@ echo -e "\n"
 #想定出力:引数チェックエラー
 ./20745106_kadai4.sh 15
 if [ $? -ne 0 ];then
-    echo -e "CASE8処理エラー"  
+    echo -e "シェル異常終了　CASE8の実行結果は想定通り"  
 else
-    echo -e "CASE8正常終了"	
+    echo -e "シェル正常終了　CASE8処理エラー" 
 fi
 echo -e "\n"
